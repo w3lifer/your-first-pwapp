36f47d26661ce6bc6e14c98ea8ff7060e6aec21b
Roman Grinyov <w3lifer@gmail.com>
2018-12-12 15:54:22 +0300

    added app.saveSelectedCities() method

--- work/scripts/app.js
+++ work/scripts/app.js
@@ -1,11 +1,11 @@
 // Copyright 2016 Google Inc.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -196,7 +196,12 @@
     });
   };
 
-  // TODO add saveSelectedCities function here
+  /**
+   * Save list of cities to localStorage
+   */
+  app.saveSelectedCities = function () {
+      localStorage.selectedCities = JSON.stringify(app.selectedCities);
+  };
 
   app.getIconClass = function(weatherCode) {
     // Weather codes: https://developer.yahoo.com/weather/documentation.html#codes
